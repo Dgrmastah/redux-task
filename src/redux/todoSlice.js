55f46524f3a92416ee92da5ff3a525248374883b
Dgrmastah <gasbertuccio@gmail.com>
2025-03-26 10:10:11 +0100

    redux

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,21 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    task:[],
+  tasks: [],
 };
 
 export const taskSlice = createSlice({
-    name:"tasks",
-    initialState,
-    reducers: {
-        addTask : (state, action) => {
-            state.task.push(action.payload);
-        },
-        removeTask : (state, action) => {
-            state.task = state.task.filter(task => task.id !== action.payload.id)
-        },
+  name: "task",
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.tasks.push(action.payload);
     },
+    removeTask: (state, action) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    },
+  },
 });
 
-export const {addTask, removeTask} = taskSlice.actions;
+export const { addTask, removeTask } = taskSlice.actions;
 export default taskSlice.reducer;
